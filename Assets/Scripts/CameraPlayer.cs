6fb39911b9c5d0f1974554a1d50a19386efb8b92
Maciej Rybak <maciejrybak99@gmail.com>
2021-12-10 12:55:32 +0100

    Update Camera and physics

--- Assets/Scripts/CameraPlayer.cs
+++ Assets/Scripts/CameraPlayer.cs
@@ -7,44 +7,88 @@ public class CameraPlayer : MonoBehaviour {
 	// Use this for initialization
 	public float speedcamera =120.0f;
 	public GameObject camerafollowobject;
-	public float clampeagle=80.0f;
+    public GameObject celownik;
+
+	//kąty maksymalne
+	private float minClampEagle=80.0f;
+    private float maxClampEagle = 60f;
+
 	public float inputSen = 150f;
 	private float mouseX;
 	private float mouseY;
-	public float fininputX;
-	public float fininputZ;
+
 	private float rotX=0f;
 	private float rotY=0f;
-	
+
+	//zmiana szybkości "jechania" kamery
+    public float speedChangeCamera = 3f;
+
+    private Quaternion defaultRotation;
+
+	//zmienna odpowiadająca za to czy przycisk jest wciśnięty
+	private bool keyDown = false;
 	void Start () 
 	{
-	Vector3 rot = transform.localRotation.eulerAngles;
-	rotY = rot.y;
-	rotX = rot.x;
-	Cursor.lockState = CursorLockMode.Locked;
-	Cursor.visible = false;
-	}
+	    Vector3 rot = transform.localRotation.eulerAngles;
+	    rotY = rot.y;
+	    rotX = rot.x;
+	    Cursor.lockState = CursorLockMode.Locked;
+	    Cursor.visible = false;
+        defaultRotation = Camera.main.transform.localRotation;
+    }
 	
 	// Update is called once per frame
-	void Update () 
-	{
-		mouseX = Input.GetAxis("Mouse X");
-		mouseY = Input.GetAxis("Mouse Y");
-		//na pada add
-		fininputX =  mouseX;
-		fininputZ = mouseY;
-		rotY += fininputX * inputSen * Time.deltaTime;
-		rotX += fininputZ * inputSen * Time.deltaTime;
-		rotX = Mathf.Clamp(rotX,-clampeagle,clampeagle);
-		
-		Quaternion localrotation = Quaternion.Euler(rotX,rotY,0f);
-		transform.rotation = localrotation;
-		
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.T)) {
+			keyDown = !keyDown;
+        }
+
+		if (keyDown) {
+			//Debug.Log("przycisk wciśnięty");
+            Camera.main.GetComponent<CollisionCamera>().enabled = false;
+
+            Camera.main.GetComponent<MouseHandler>().enabled = true;
+			//ustawianie pozycji kamery na środek
+
+			Camera.main.transform.position =
+                Vector3.Lerp(Camera.main.transform.position, celownik.transform.position,
+                    Time.deltaTime * speedChangeCamera);
+
+        }
+		else {
+            //Debug.Log("przycisk nie wciśnięty");
+            Camera.main.GetComponent<MouseHandler>().enabled = false;
+            //przywracanie rotacji domyślnej
+            Camera.main.transform.localRotation = defaultRotation;
+			Camera.main.GetComponent<CollisionCamera>().enabled = true;
+			FreeCamera();
+        }
 	}
-	void LateUpdate()
+
+ 
+
+
+    void LateUpdate()
 	{
-		camerafollow();
+        if (!keyDown) {
+			//Debug.Log("lateUpdate");
+            camerafollow();
+        }
+    }
+
+    private void FreeCamera() {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * inputSen * Time.deltaTime;
+        rotX += mouseY * inputSen * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -minClampEagle, maxClampEagle);
+
+        Quaternion localrotation = Quaternion.Euler(rotX, rotY, 0f);
+        transform.rotation = localrotation;
 	}
+
 	void camerafollow()
 	{
 		Transform cel = camerafollowobject.transform;
--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -1,127 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Profiling;
 using UnityEngine;
 
-public class move : MonoBehaviour {
+public class move : MonoBehaviour
+{
 
-	// Use this for initialization
-	
-	public GameObject cam;
-	private CharacterController con;
-	private Transform tran;
-	private float rotationspeed;
+    // Use this for initialization
 
-	void Start () 
-	{
-		con = GetComponent<CharacterController>();
-		tran = GetComponent<Transform>();
-	}
-	
-	float inputX;
-	float InputZ;
-	private Vector3 MoveDirection;
-	public bool rotationPlayer;
-	private float speed;
-	public float verticallevel;
-	private Vector3 movevector;
-	private float allowPlayerRotation;
-	private float sp;
-	public float speedrun = 3f;
+    private float rotationSpeed = 0.1f;
 
-	public float sprint=6f;
-	
-	void Update () 
-	{
-	
-		setmove();
-		if(con.isGrounded)
-		{
-			verticallevel -= 0;
-			verticallevel = 0;
-		}
-		else
-		{
-			verticallevel -= 6;
-		}
-		if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D))
-		{
-			sp = speedrun;
-		}
-		else
-		{
-			sp = 0f;
-		}
-		if(Input.GetKey(KeyCode.W)&&Input.GetKey(KeyCode.LeftShift)||Input.GetKey(KeyCode.S)&&Input.GetKey(KeyCode.LeftShift)||Input.GetKey(KeyCode.A)&&Input.GetKey(KeyCode.LeftShift)||Input.GetKey(KeyCode.D)&&Input.GetKey(KeyCode.LeftShift))
-		{
-			sp = sprint;
-		}
-		
-		movevector = transform.TransformDirection(new Vector3(0f,verticallevel,sp)*Time.deltaTime);
-		con.Move(movevector);
-		
-	
-	}
+    private CharacterController characterController;
+    public float movmentSpeed = 1f;
 
-	void UpdatePositionAndRotationPlayer()
-	{
-		inputX = Input.GetAxis("Horizontal");
-		InputZ = Input.GetAxis("Vertical");
+    public float normalMovementSpeed = 3f;
+    public float runMovementSpeed = 8f;
+    public float gravity = 0.00005f;
+    public float velocity = 0f;
 
-		var camera = cam;
-		var foward = cam.transform.forward;
-		var right = cam.transform.right;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
 
-		foward.y=0f;
-		right.y=0f;
+    private float inputX;
+    private float inputZ;
+    private Vector3 foward;
+    private Vector3 right;
 
-		foward.Normalize();
-		right.Normalize();
 
-		MoveDirection = foward * InputZ + right * inputX;
-		if(rotationPlayer == false)
-		{
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(MoveDirection), rotationspeed);
-		}
-	}
 
-	void setmove()
-	{
-		inputX = Input.GetAxis("Horizontal");
-		InputZ = Input.GetAxis("Vertical");
-		speed = new Vector2(inputX,InputZ).sqrMagnitude;
-		if(speed > allowPlayerRotation)
-		{
-			UpdatePositionAndRotationPlayer();
-		}
+    void Update()
+    {
+        //ruch postaci wedle ułożenia kamery
+        inputX = Input.GetAxis("Horizontal") * movmentSpeed;
+        inputZ = Input.GetAxis("Vertical") * movmentSpeed;
 
-	}
+        foward = Camera.main.transform.forward;
+        right = Camera.main.transform.right;
 
-	/*	mouseY =Input.GetAxis("Mouse Y");
-		d
-	
-		if(con.isGrounded)
-		{
-			vector = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-			vector = transform.TransformDirection(vector);
-			vector *= speed;		
-			
-		}
-		vector.y -= 20f*Time.deltaTime;
-		if(Input.GetKey(KeyCode.LeftShift))
-		{	
-			con.Move(vector*2 * Time.deltaTime);
-		}	
-		else
-		{
-			
-			con.Move(vector * Time.deltaTime);
-		}*/
-		
+        foward.y = 0f;
+        right.y = 0f;
 
-			
-		
-	
+        foward.Normalize();
+        right.Normalize();
 
-		
-	
+        Vector3 MoveDirection = foward * inputZ + right * inputX;
+
+        characterController.Move(MoveDirection * Time.deltaTime);
+
+
+
+        //grawitacja
+        if (characterController.isGrounded)
+        {
+            velocity = 0;
+        }
+        else if (velocity > -0.08) {
+            velocity -= gravity * (float) 0.0001;
+        }
+
+
+        characterController.Move(new Vector3(0, velocity, 0));
+
+        UpdateRotationObject();
+
+    }
+
+    void UpdateRotationObject()
+    {
+        var speed = new Vector2(inputX, inputZ).sqrMagnitude;
+        //Debug.Log(speed);
+        if (speed > 0) {
+            var moveDirection = foward * inputZ + right * inputX;
+            transform.rotation =
+                Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed);
+           
+        }
+    }
+
+
+
+    /*    
+     *    
+     *    
+     *    void setmove()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+        speed = new Vector2(inputX,InputZ).sqrMagnitude;
+        if(speed > allowPlayerRotation)
+        {
+            UpdatePositionAndRotationPlayer();
+        }
+
+    }
+     *    
+     *    mouseY =Input.GetAxis("Mouse Y");
+        d
+    
+        if(con.isGrounded)
+        {
+            vector = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+            vector = transform.TransformDirection(vector);
+            vector *= speed;        
+            
+        }
+        vector.y -= 20f*Time.deltaTime;
+        if(Input.GetKey(KeyCode.LeftShift))
+        {    
+            con.Move(vector*2 * Time.deltaTime);
+        }    
+        else
+        {
+            
+            con.Move(vector * Time.deltaTime);
+        }*/
+
+
+
+
+
+
+
+
 }
