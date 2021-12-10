580ae442761b0fb577c0205a4fc2d4b8d8d264b8
Maciej Rybak <maciejrybak99@gmail.com>
2021-12-10 23:22:22 +0100

    Update Camera

--- Assets/Scripts/CameraPlayer.cs
+++ Assets/Scripts/CameraPlayer.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class CameraPlayer : MonoBehaviour {
 
 	// Use this for initialization
-	public float speedcamera =120.0f;
-	public GameObject camerafollowobject;
+    public GameObject camerafollowobject;
     public GameObject celownik;
 
 	//kąty maksymalne
@@ -23,8 +22,11 @@ public class CameraPlayer : MonoBehaviour {
 	//zmiana szybkości "jechania" kamery
     public float speedChangeCamera = 3f;
 
+    //domyślna rotacja
     private Quaternion defaultRotation;
 
+
+    public bool endAiminng = true;
 	//zmienna odpowiadająca za to czy przycisk jest wciśnięty
 	private bool keyDown = false;
 	void Start () 
@@ -42,23 +44,36 @@ public class CameraPlayer : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.T)) {
 			keyDown = !keyDown;
+            rotX = celownik.transform.parent.eulerAngles.x;
+            rotY = celownik.transform.parent.eulerAngles.y;
+            endAiminng = false;
+            //ustawianie pozycji kamery na środek
         }
 
+		
 		if (keyDown) {
 			//Debug.Log("przycisk wciśnięty");
             Camera.main.GetComponent<CollisionCamera>().enabled = false;
+            //Jeżeli kamera się ustawi do pozycji ma się odblokować możliwość ruszania kamerą
+            if(Vector3.Distance(Camera.main.transform.position, celownik.transform.position) < 0.1f) { 
+                endAiminng = true;
+                AimingCamera();
 
-            Camera.main.GetComponent<MouseHandler>().enabled = true;
-			//ustawianie pozycji kamery na środek
+            }
+            //do momentu aż nie zostanie ustanowiona pozycja
+            else if(!endAiminng) {
+                //obrót rotacji
+                Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation,
+                    celownik.transform.parent.rotation, 0.05f);
 
-			Camera.main.transform.position =
-                Vector3.Lerp(Camera.main.transform.position, celownik.transform.position,
-                    Time.deltaTime * speedChangeCamera);
-
+                //zmiana pozycji
+                Camera.main.transform.position =
+                    Vector3.Lerp(Camera.main.transform.position, celownik.transform.position,
+                        Time.deltaTime * speedChangeCamera);
+            }
         }
 		else {
             //Debug.Log("przycisk nie wciśnięty");
-            Camera.main.GetComponent<MouseHandler>().enabled = false;
             //przywracanie rotacji domyślnej
             Camera.main.transform.localRotation = defaultRotation;
 			Camera.main.GetComponent<CollisionCamera>().enabled = true;
@@ -77,7 +92,20 @@ public class CameraPlayer : MonoBehaviour {
         }
     }
 
-    private void FreeCamera() {
+    private void AimingCamera() {
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        Debug.Log(rotY);
+        rotY += mouseX * inputSen * Time.deltaTime;
+        rotX += mouseY * inputSen * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -50, 50);
+        rotY = Mathf.Clamp(rotY, celownik.transform.parent.eulerAngles.y-40, celownik.transform.parent.eulerAngles.y + 40);
+        Camera.main.transform.eulerAngles = new Vector3(rotX, rotY, 0f);
+
+	}
+
+	private void FreeCamera() {
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
 
@@ -92,7 +120,7 @@ public class CameraPlayer : MonoBehaviour {
 	void camerafollow()
 	{
 		Transform cel = camerafollowobject.transform;
-		float step = speedcamera * Time.deltaTime;
+		float step = 120f * Time.deltaTime;
 		transform.position = Vector3.MoveTowards(transform.position,cel.position,step);
 	}
 }
